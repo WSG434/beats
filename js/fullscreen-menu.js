b7bba46710924b097ca8f1795a88b388914c1808
WSG434 <WorkStationGuard@gmail.com>
2022-06-27 09:25:17 +0300

    Сделал появление/пропадание полноэкранного меню

---
+++ js/fullscreen-menu.js
@@ -0,0 +1,25 @@
+const openButton = document.querySelector(".hamburger");
+const fullscreen = document.querySelector(".wrapper-fullscreen")
+const body = document.body;
+
+openButton.addEventListener("click", e => {
+  fullscreen.style.display = "flex";
+})
+
+
+const overlayElement = document.querySelector(".wrapper-fullscreen");
+const overlayElement2 = document.querySelector(".fullscreen-menu");
+overlayElement.addEventListener("click", e => {
+  console.log(e.target);
+  if (e.target === overlayElement || e.target === overlayElement2) {
+    closeElement.click();
+    fullscreen.style.display = "none";
+  }
+})
+
+const closeElement = overlayElement.querySelector(".close");
+closeElement.addEventListener("click", e => {
+  e.preventDefault();
+  fullscreen.style.display = "none";
+})
+

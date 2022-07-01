48b2ecfa4826518998a2a7fb2659a14249360f3d
WSG434 <WorkStationGuard@gmail.com>
2022-07-01 21:49:09 +0300

    Подключил bxSlider и реализовал функционирование слайдера;

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+// $('.slider__list').bxSlider({
+//   pager: false,
+//   controls: false
+// });
+
+const slider = $('.slider__list').bxSlider({
+  pager: false
+  // controls: false
+});
+
+$('.arrow--left').click((e) => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+})
+
+
+$('.arrow--right').click((e) => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})

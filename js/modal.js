3b6b1204b0c42e91bccf4bf713488c3943125740
WSG434 <WorkStationGuard@gmail.com>
2022-07-04 08:27:42 +0300

    JS; Реализовано модальное окно на форме; Отправка/обработка запроса и ответа от сервера;

--- js/modal.js
+++ js/modal.js
@@ -6,25 +6,61 @@ $('.form').submit (e => {
   const phone = form.find("[name='phone']");
   const comment = form.find("[name='comment']");
   const to = form.find("[name='to']");
+
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+
+
+
+  const validateFields = (form, fieldsArray) => {
+    fieldsArray.forEach((field) => {
+        field.removeClass("input-error");
+        if (field.val().trim() === "") {
+          field.addClass("input-error");
+        }
+    })
+
+    const errorFields = form.find(".input-error");
+
+    return errorFields.length === 0;
+  }
+
+  const isValid = validateFields(form, [name, phone, comment, to]);  
+
+  if (isValid){
+    const request = $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      }
+    });
   
-  $.ajax({
-    url: "https://webdev-api.loftschool.com/sendmail",
-    method: "post",
-    data: {
-      name: name.val(),
-      phone: phone.val(),
-      comment: comment.val(),
-      to: to.val()
-    }
+    request.done((data) => {
+      content.text(data.message);
+    });
+      
+    request.fail((data) => {
+      const message = data.responseJSON.message;
+        content.text(message);
+        modal.addClass("error-modal");
+    })
 
-  })
-
-  $.fancybox.open ({
-    src: "#modal",
-    type: "inline"
-  })
-
+    request.always(() => {
+      $.fancybox.open ({
+          src: "#modal",
+          type: "inline"
+        });
+    });
+  
+  }
 });
+
 
 
 $(".js-submit-btn").click(e => {

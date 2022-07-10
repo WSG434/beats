b3b5754a6b7a75b8fad3071fd29e57438e3d614a
WSG434 <WorkStationGuard@gmail.com>
2022-07-10 17:17:00 +0300

    вернул видеоплеер

--- js/player.js
+++ js/player.js
@@ -1,18 +1,18 @@
-// let player;
-//       function onYouTubeIframeAPIReady() {
-//         player = new YT.Player('yt-player', {
-//           height: '405',
-//           width: '660',
-//           videoId: 'V2i1YkfrM54',
-//           events: {
-//             // 'onReady': onPlayerReady,
-//             // 'onStateChange': onPlayerStateChange
-//           },
-//           playerVars:{
-//             showinfo: 0,
-//             rel: 0,
-//             autoplay: 0
-//           }
+let player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('yt-player', {
+          height: '405',
+          width: '660',
+          videoId: 'V2i1YkfrM54',
+          events: {
+            // 'onReady': onPlayerReady,
+            // 'onStateChange': onPlayerStateChange
+          },
+          playerVars:{
+            showinfo: 0,
+            rel: 0,
+            autoplay: 0
+          }
           
-//         });
-//       }
+        });
+      }

854f962c1be92bfaf00cf2c37b5ad761c5754e5c
WSG434 <WorkStationGuard@gmail.com>
2022-07-15 10:23:32 +0300

    js; кастомный плеер HTML5 Video API

--- js/player.js
+++ js/player.js
@@ -17,129 +17,118 @@
 //         });
 //       }
 
-
 // LoftSchool (youtube iframe api)
-let player;
-const playerContainer = $('.player');
+// let player;
+// const playerContainer = $('.player');
 
-let eventsInit = () => {
-  $(".player__start").click(e => {
-    e.preventDefault();
+// let eventsInit = () => {
+//   $(".player__start").click(e => {
+//     e.preventDefault();
 
-    if (playerContainer.hasClass("paused")){
-      player.pauseVideo();
-    }else {
-      player.playVideo();
-    }
-  });
+//     if (playerContainer.hasClass("paused")){
+//       player.pauseVideo();
+//     }else {
+//       player.playVideo();
+//     }
+//   });
 
-  $(".player__playback").click(e => {
-    const bar = $(e.currentTarget);
-    const clickedPosition = e.originalEvent.layerX;
-    const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
-    const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
+//   $(".player__playback").click(e => {
+//     const bar = $(e.currentTarget);
+//     const clickedPosition = e.originalEvent.layerX;
+//     const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+//     const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
 
-    $(".player__playback-button").css({
-      left: `${newButtonPositionPercent}%`
-    });
+//     $(".player__playback-button").css({
+//       left: `${newButtonPositionPercent}%`
+//     });
 
-    player.seekTo(newPlaybackPositionSec);
-    
-  });
+//     player.seekTo(newPlaybackPositionSec);
 
-  $(".player__splash").click(e => {
-    player.playVideo();
-  });
-};
+//   });
 
-const formatTime = timeSec => {
-  const roundTime = Math.round(timeSec);
+//   $(".player__splash").click(e => {
+//     player.playVideo();
+//   });
+// };
 
-  const minutes = addZero(Math.floor(roundTime/60));
-  const seconds = addZero(roundTime - minutes*60);
+// const formatTime = timeSec => {
+//   const roundTime = Math.round(timeSec);
 
-  function addZero(num){
-    return num < 10 ? `0${num}` : num;
-  }
-  
-  return `${minutes}:${seconds}`;
-}
+//   const minutes = addZero(Math.floor(roundTime/60));
+//   const seconds = addZero(roundTime - minutes*60);
 
+//   function addZero(num){
+//     return num < 10 ? `0${num}` : num;
+//   }
 
-const onPlayerReady = () => {
-  let interval;
-  const durationSec = player.getDuration();
+//   return `${minutes}:${seconds}`;
+// }
 
-  $(".player__duration-estimate").text(formatTime(durationSec));
+// const onPlayerReady = () => {
+//   let interval;
+//   const durationSec = player.getDuration();
 
-  if (typeof intreval !== 'undefined') {
-    clearInterval(interval);
-  }
+//   $(".player__duration-estimate").text(formatTime(durationSec));
 
-  intreval = setInterval(() => {
-    const completedSec = player.getCurrentTime();
-    const completedPercent = (completedSec/durationSec) * 100;
+//   if (typeof intreval !== 'undefined') {
+//     clearInterval(interval);
+//   }
 
-    $(".player__playback-button").css({
-      left: `${completedPercent}%`
-    })
-    $(".player__duration-completed").text(formatTime(completedSec));
-  }, 1000);
-}
+//   intreval = setInterval(() => {
+//     const completedSec = player.getCurrentTime();
+//     const completedPercent = (completedSec/durationSec) * 100;
 
-const onPlayerStateChange = event => {
-  /*
-  -1 (воспроизведение видео не начато)
-  0 (воспроизведение видео завершено)
-  1 (воспроизведение)
-  2 (пауза)
-  3 (буферизация)
-  5 (видео подают реплики).
-  */
-  switch (event.data) {
-    case 1:
-      playerContainer.addClass("active");
-      playerContainer.addClass("paused");
-      break;
-    
-    case 2: 
-      playerContainer.removeClass("active");
-      playerContainer.removeClass("paused");
-      break;
-  }
+//     $(".player__playback-button").css({
+//       left: `${completedPercent}%`
+//     })
+//     $(".player__duration-completed").text(formatTime(completedSec));
+//   }, 1000);
+// }
 
-}
+// const onPlayerStateChange = event => {
+//   /*
+//   -1 (воспроизведение видео не начато)
+//   0 (воспроизведение видео завершено)
+//   1 (воспроизведение)
+//   2 (пауза)
+//   3 (буферизация)
+//   5 (видео подают реплики).
+//   */
+//   switch (event.data) {
+//     case 1:
+//       playerContainer.addClass("active");
+//       playerContainer.addClass("paused");
+//       break;
 
+//     case 2:
+//       playerContainer.removeClass("active");
+//       playerContainer.removeClass("paused");
+//       break;
+//   }
 
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('yt-player', {
-          height: '405',
-          width: '660',
-          videoId: 'KZZB-QSsEFg',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          },
-          playerVars:{
-            controls: 0,
-            disablekb: 0,
-            showinfo: 0,
-            rel: 0,
-            autoplay: 0,
-            modestbranding: 0
-          }
-        });
-      }
+// }
 
-eventsInit();
+//       function onYouTubeIframeAPIReady() {
+//         player = new YT.Player('yt-player', {
+//           height: '405',
+//           width: '660',
+//           videoId: 'KZZB-QSsEFg',
+//           events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange
+//           },
+//           playerVars:{
+//             controls: 0,
+//             disablekb: 0,
+//             showinfo: 0,
+//             rel: 0,
+//             autoplay: 0,
+//             modestbranding: 0
+//           }
+//         });
+//       }
 
-
-
-
-
-
-      
-
+// eventsInit();
 
 // HTML5 Video API
 
@@ -160,7 +149,6 @@ eventsInit();
 
 // play.addEventListener('click', playPauseMedia);
 
-
 // function playPauseMedia() {
 //   if(media.paused) {
 //     play.setAttribute('data-icon','u');
@@ -174,7 +162,6 @@ eventsInit();
 // stop.addEventListener('click', stopMedia);
 // media.addEventListener('ended', stopMedia);
 
-
 // function stopMedia() {
 //   media.pause();
 //   media.currentTime = 0;
@@ -183,7 +170,6 @@ eventsInit();
 
 // rwd.addEventListener('click', mediaBackward);
 // fwd.addEventListener('click', mediaForward);
-
 
 // let intervalFwd;
 // let intervalRwd;
@@ -254,7 +240,6 @@ eventsInit();
 //   timerBar.style.width = `${barLength}px`;
 // }
 
-
 // rwd.classList.remove('active');
 // fwd.classList.remove('active');
 // clearInterval(intervalRwd);
@@ -263,3 +248,118 @@ eventsInit();
 // document.onclick = function(e) {
 //   console.log(e.x) + ',' + console.log(e.y)
 // }
+
+// LoftSchool + HTML5
+
+const vidWrapper = document.querySelector("div.player");
+const myVid = vidWrapper.querySelector("video");
+
+const controlPlay = vidWrapper.querySelector(".player__start");
+
+let player;
+const playerContainer = $(".player");
+
+let eventsInit = () => {
+  $(".player__start").click((e) => {
+    e.preventDefault();
+
+    if (playerContainer.hasClass("paused")) {
+      myVid.pause();
+      playerContainer.removeClass("active");
+      playerContainer.removeClass("paused");
+      controlPlay.innerHTML = "❚❚";
+    } else {
+      myVid.play();
+      playerContainer.addClass("active");
+      playerContainer.addClass("paused");
+      controlPlay.innerHTML = "►";
+    }
+  });
+
+  $(".player__splash").click((e) => {
+    if (!playerContainer.hasClass("paused")) {
+      myVid.play();
+      playerContainer.addClass("active");
+      playerContainer.addClass("paused");
+      controlPlay.innerHTML = "►";
+    }
+  });
+
+  $(".player__elem").click((e) => {
+    if (playerContainer.hasClass("paused")) {
+      myVid.pause();
+      playerContainer.removeClass("active");
+      playerContainer.removeClass("paused");
+      controlPlay.innerHTML = "❚❚";
+    }
+  });
+};
+
+$(".player__playback").click((e) => {
+  const bar = $(e.currentTarget);
+  const clickedPosition = e.originalEvent.layerX;
+  const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+  const newPlaybackPositionSec =
+    (myVid.duration / 100) * newButtonPositionPercent;
+
+  $(".player__playback-button").css({
+    left: `${newButtonPositionPercent}%`,
+  });
+
+  myVid.currentTime = newPlaybackPositionSec;
+});
+
+$(".player__volume").click((e) => {
+  const bar = $(e.currentTarget);
+  const clickedPosition = e.originalEvent.layerX;
+  let newButtonPositionPercent = clickedPosition / bar.width();
+  if (newButtonPositionPercent < 0) {
+    newButtonPositionPercent = 0;
+  }
+
+  $(".player__volume-button").css({
+    left: `${newButtonPositionPercent * 100}%`,
+  });
+  $(".player__volume--filled").css({
+    width: `${newButtonPositionPercent * 100}%`,
+  });
+
+  myVid.volume = newButtonPositionPercent;
+});
+
+const formatTime = (timeSec) => {
+  const roundTime = Math.round(timeSec);
+
+  const minutes = addZero(Math.floor(roundTime / 60));
+  const seconds = addZero(roundTime - minutes * 60);
+
+  function addZero(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  return `${minutes}:${seconds}`;
+};
+
+const progressBar = () => {
+  let interval;
+  const durationSec = myVid.duration;
+
+  if (typeof interval !== "undefined") {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    const completedSec = myVid.currentTime;
+    const completedPercent = (completedSec / durationSec) * 100;
+
+    $(".player__playback-button").css({
+      left: `${completedPercent}%`,
+    });
+    $(".player__playback--filled").css({
+      width: `${completedPercent}%`,
+    });
+  }, 1000);
+};
+
+eventsInit();
+progressBar();

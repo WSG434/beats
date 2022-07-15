b72aee308bedd7f9e9a3f81176b9685dd0726178
WSG434 <WorkStationGuard@gmail.com>
2022-07-15 22:02:53 +0300

    fix; поправил ошибку со слайдером

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -3,23 +3,19 @@
 //   controls: false
 // });
 
-
-
-const slider = $('.slider__list').bxSlider({
+const slider = $(".slider__list").bxSlider({
   //https://bxslider.com/install/
   pager: false,
   controls: false,
-  touchEnabled: false
-
+  // touchEnabled: false
 });
 
-$('.arrow--left').click((e) => {
+$(".arrow--left").click((e) => {
   e.preventDefault();
   slider.goToPrevSlide();
-})
+});
 
-
-$('.arrow--right').click((e) => {
+$(".arrow--right").click((e) => {
   e.preventDefault();
   slider.goToNextSlide();
-})
+});
